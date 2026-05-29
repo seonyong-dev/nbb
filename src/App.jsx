a567ee06f81fcc1b2dcb1c22ed08bd4e1cc445c4
unknown <tjsdyd0911@gmail.com>
2026-05-29 15:53:34 +0900

    feat: StatusTemplate 올바른 역할을 위한 각각의 StatusPage 추가

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import './App.css';
 import Sidebar from './components/Sidebar';
 import Login from './pages/Login';
 import GroupList from './pages/GroupList';
-import SettlementStatusPage from './pages/SettlementStatusPage'; // components폴더에서 template 가져오는걸로 수정해야함!!
+import Paid from './pages/PaidStatusPage';
+import Settlement from './pages/SettlementStatusPage';
 import GroupInside from './pages/GroupInside';
 
-// 1. 레이아웃 컴포넌트: 사이드바가 들어갈 틀만 정의합니다. (useLocation 삭제)
 const SidebarLayout = () => {
   return (
     <div className="container-fluid">
@@ -26,14 +26,14 @@ function App() {
   return (
     <Router>
       <Routes>
-        {/* 2. 사이드바가 없는 독립적인 페이지 (예: 로그인) */}
+        {/* 사이드바가 없는 독립적인 페이지*/}
         <Route path="/" element={<Login />} />
 
-        {/* 3. 사이드바를 공유하는 페이지들을 그룹으로 묶음 */}
+        {/* 사이드바를 공유하는 페이지 */}
         <Route element={<SidebarLayout />}>
           <Route path="/group-list" element={<GroupList />} />
-          {/* SettlementStatusPage-> StatusTemplate 으로 수정해야함 */}
-          <Route path="/SettlementStatusPage" element={<SettlementStatusPage />} />
+          <Route path="/Paid" element={<Paid />} />
+          <Route path="/Settlement" element={<Settlement />} />
           <Route path="/group-inside" element={<GroupInside />} />
         </Route>
       </Routes>
