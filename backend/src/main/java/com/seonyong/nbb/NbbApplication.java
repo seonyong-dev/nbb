2658223e69b9dcc5cc15f2fca5828866c9dcd841
unknown <tjsdyd0911@gmail.com>
2026-05-31 17:25:42 +0900

    feat: 스프링부트 설정 및 DB연결 설정, frontend 폴더 추가하여 backend와 파일과 분리

---
+++ backend/src/main/java/com/seonyong/nbb/NbbApplication.java
@@ -0,0 +1,13 @@
+package com.seonyong.nbb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbbApplication.class, args);
+	}
+
+}
