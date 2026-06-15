f35ed56eadea03ba942628ea406e285c91b6459c
unknown <tjsdyd0911@gmail.com>
2026-06-16 01:12:33 +0900

    feat: Repository 완료

---
+++ backend/src/main/java/com/seonyong/nbb/domain/paid/repository/PaidRepository.java
@@ -0,0 +1,20 @@
+package com.seonyong.nbb.domain.paid.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.seonyong.nbb.global.entity.Settlement;
+
+public interface PaidRepository extends JpaRepository<Settlement, Long> {
+    
+    // 송금현황 리스트 불러오기
+    @Query("SELECT DISTINCT s FROM Settlement s " +
+           "JOIN s.senderMemberId m " +
+           "JOIN FETCH s.expenseId e " +
+           "JOIN FETCH m.groupId " +
+           "WHERE s.senderMemberId = :memberId ")
+    List<Settlement> findBySettlement(@Param("memberId")Long memberId);
+}
